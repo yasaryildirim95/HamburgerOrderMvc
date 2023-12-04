ab7f9e6cbf6a38a7d4267caf84a473218e73b3c5
yasaryildirim95 <yasaryildirim95@gmail.com>
2023-12-04 22:23:22 +0300

    LoginPartialAdded

---
+++ BurgerOrderMVC/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BurgerOrderMVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Order()
+        {
+
+            List<string> extras = new List<string>();
+            extras.Add("barbekü");
+            extras.Add("ranch");
+            List<string> menus = new List<string>();
+            menus.Add("whoper");
+            menus.Add("stek");
+
+
+
+
+
+            ViewBag.Extras = extras;
+            ViewBag.Menus = menus;
+
+
+            return View();
+        }
+    }
+}
