6657e746518c7c00e2c9e01e5e036bf4fc7ddf19
OzanUlus <ilkerozanulus@gmail.com>
2023-12-06 11:11:20 +0300

    solved some peroblems

--- BurgerOrderBLL/Extension/BLDepencies.cs
+++ BurgerOrderBLL/Extension/BLDepencies.cs
@@ -20,10 +20,10 @@ namespace BurgerOrderBLL.Extension
 
 
             service.AddScoped<IAccountService, AccountManager>();
-            //service.AddScoped<IExtrasService,ExtrasManager>();
-            //service.AddScoped<IMenuService,MenuManager>();
-            //service.AddScoped<IOrderService,OrderManager>();
-            //service.AddScoped<ISubstanceService,SubstanceManager>();
+            service.AddScoped<IExtrasService, ExtrasManager>();
+            service.AddScoped<IMenuService, MenuManager>();
+            service.AddScoped<IOrderService, OrderManager>();
+            service.AddScoped<ISubstanceService, SubstanceManager>();
 
 
 
